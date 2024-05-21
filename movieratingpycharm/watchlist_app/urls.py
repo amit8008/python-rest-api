49db6014cf934c1c27a0f255868c7a76d9327c69
Amit Singh <amit8singh008@gmail.com>
2024-05-22 00:29:45 +0530

    create 1st url to get movie list

--- movieratingpycharm/watchlist_app/urls.py
+++ movieratingpycharm/watchlist_app/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path, include
+from watchlist_app.views import movie_list
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('movie/', include('watchlist_app.urls')),
+    path('list/', movie_list, name= 'movie-list'),
 ]
