ee11a13822ea0c9a3a08e1a873b4ac80d3e8ef77
Amit Singh <amit8singh008@gmail.com>
2024-05-24 02:07:35 +0530

    add validator for description of movie

--- movieratingpycharm/watchlist_app/api/serializers.py
+++ movieratingpycharm/watchlist_app/api/serializers.py
@@ -2,30 +2,34 @@ from rest_framework import serializers
 from watchlist_app.models import Movie
 
 
-class MovieSerializers(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+def length_gt_4(value):
+    if len (value) < 4:
+        raise serializers.ValidationError ("Please provide proper movie description !")
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
+class MovieSerializers (serializers.Serializer):
+    id = serializers.IntegerField (read_only=True)
+    name = serializers.CharField ()
+    description = serializers.CharField (validators=[length_gt_4])
+    active = serializers.BooleanField ()
+
+    def create(self,validated_data):
+        return Movie.objects.create (**validated_data)
+
+    def update(self,instance,validated_data):
+        instance.name = validated_data.get ('name',instance.name)
+        instance.description = validated_data.get ('description',instance.description)
+        instance.active = validated_data.get ('active',instance.active)
+        instance.save ()
         return instance
 
     # Field-level validation
-    def validate_name(self, value):
-        if len(value) < 3:
-            raise serializers.ValidationError("Name is too short")
+    def validate_name(self,value):
+        if len (value) < 3:
+            raise serializers.ValidationError ("Name is too short")
         return value
 
-    def validate(self, data):
+    def validate(self,data):
         if data['name'] == data['description']:
-            raise serializers.ValidationError("Movie name should not same as description")
+            raise serializers.ValidationError ("Movie name should not same as description")
         return data
-
