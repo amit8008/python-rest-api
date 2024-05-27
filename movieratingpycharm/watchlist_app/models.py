1fee4e1ec216b89021ec128fc264c433a86791f3
Amit Singh <amit8singh008@gmail.com>
2024-05-28 00:34:05 +0530

    connect shows models with stream platform as a field

--- movieratingpycharm/watchlist_app/models.py
+++ movieratingpycharm/watchlist_app/models.py
@@ -2,16 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-class Shows(models.Model) :
-    title = models.CharField(max_length = 50)
-    story_line = models.CharField(max_length = 200)
-    active = models.BooleanField(default = True)
-    created = models.DateTimeField(auto_now_add = True)
-
-    def __str__(self) :
-        return self.title
-
-
 class StreamPlatform(models.Model) :
     name = models.CharField(max_length = 30)
     about = models.CharField(max_length = 150)
@@ -20,6 +10,16 @@ class StreamPlatform(models.Model) :
     def __str__(self) :
         return self.name
 
+
+class Shows(models.Model) :
+    title = models.CharField(max_length = 50)
+    story_line = models.CharField(max_length = 200)
+    active = models.BooleanField(default = True)
+    created = models.DateTimeField(auto_now_add = True)
+    platform = models.ForeignKey(StreamPlatform, on_delete = models.CASCADE, related_name = "All_Shows")
+
+    def __str__(self) :
+        return self.title
 
 # class Movie(models.Model):
 #     name = models.CharField(max_length=50)
